32076daaf2d409af214178f8aa17e56eb01daea1
gadZella <zelick@list.ru>
2017-11-04 13:22:40 +0300

    FragmentType.Unknown is never used

--- FileModel/FragmentType.py
+++ FileModel/FragmentType.py
@@ -2,9 +2,8 @@ from enum import Enum
 
 
 class FragmentType(Enum):
-    Unknown = 0,
-    Body = 1
-    IfStatement = 2
-    ElIfStatement = 3
-    ElseStatement = 4
-    EndIfStatement = 5
+    Body = 0
+    IfStatement = 1
+    ElIfStatement = 2
+    ElseStatement = 3
+    EndIfStatement = 4
