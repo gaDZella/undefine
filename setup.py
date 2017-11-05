4bb61b3d41aa762041cad9b67af4d5091dbada74
DESKTOP-L8REHGG\Panda <1@1.1>
2017-11-05 21:37:04 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="clean-sharp",
+    version="0.0.1",
+    description="C# Preprocessor directive cleaner",
+    install_requires=["joblib", "chardet", "sympy"]
+)
