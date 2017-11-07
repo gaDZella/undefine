e759e976c9c35697a7da61bba804860871f9b0ce
gadZella <zelick@list.ru>
2017-11-07 23:28:26 +0300

    refactor FileModel: fix < 3.6 build

--- FileModel/FragmentType.py
+++ FileModel/FragmentType.py
@@ -1,9 +1,9 @@
-from enum import Enum, auto
+from enum import Enum
 
 
 class FragmentType(Enum):
-    Body = auto()
-    IfStatement = auto()
-    ElIfStatement = auto()
-    ElseStatement = auto()
-    EndIfStatement = auto()
+    Body = 0
+    IfStatement = 1
+    ElIfStatement = 2
+    ElseStatement = 3
+    EndIfStatement = 4
